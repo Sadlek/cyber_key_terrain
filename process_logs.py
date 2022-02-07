592c66258216a8b996233c81e853dbb900b2db50
Sadlek <445581@mail.muni.cz>
2022-02-07 16:33:31 +0100

    Process logs updated.

--- process_logs.py
+++ process_logs.py
@@ -12,16 +12,52 @@ def filter_device(device_name):
                     output_file.write("/n")
 
 
-# "10.7.101.12"
-def filter_ip_from_syslog(ip_address, input_file='data/data.json'):
+def filter_syslog_events(list_of_ip_addresses, input_file='data/data_syslog.json'):
     output_lines = []
-    with open(f'data_{ip_address}.json', 'w') as output_file:
+    with open(f'data_filtered/data_syslog_filtered.json', 'w') as output_file:
         with open(input_file, 'r') as jsonfile:
             for line in jsonfile.readlines():
                 data = json.loads(line)
-                if data["fromhost_ip"] == ip_address:
+                if data["fromhost_ip"] in list_of_ip_addresses:
                     output_lines.append(data)
         json.dump(output_lines, fp=output_file)
+
+
+def filter_winlog_events(list_of_ip_addresses, input_file='data/data_winlog.json'):
+    output_lines = []
+    with open(f'data_filtered/data_winlog_filtered.json', 'w') as output_file:
+        with open(input_file, 'r') as jsonfile:
+            for line in jsonfile.readlines():
+                data = json.loads(line)
+                if data["host_ip"] in list_of_ip_addresses:
+                    output_file.write(line)
+                    # output_lines.append(data)
+        # json.dump(output_lines, fp=output_file)
+
+
+def filter_ip_flows(list_of_ip_addresses, input_file='data/data_ipflow.json'):
+    output_lines = []
+    with open(f'data_filtered/data_ipflow_filtered.json', 'w') as output_file:
+        with open(input_file, 'r') as jsonfile:
+            for line in jsonfile.readlines():
+                data = json.loads(line)
+                if data["destinationIPv4Address"] in list_of_ip_addresses or \
+                        data["sourceIPv4Address"] in list_of_ip_addresses:
+                    # output_lines.append(data)
+                    output_file.write(line)
+        # json.dump(output_lines, fp=output_file)
+
+
+def filter_ip(ip_address):
+    output_lines = []
+    with open(f'data_{ip_address}.json', 'w') as output_file:
+        with open('data/data.json', 'r') as jsonfile:
+            for line in jsonfile.readlines():
+                data = json.loads(line)
+                if data["destinationIPv4Address"] == ip_address or data["sourceIPv4Address"] == ip_address:
+                    # output_lines.append(data)
+                    output_file.write(line)
+        # json.dump(output_lines, fp=output_file)
 
 
 # syslog - "10.7.101.12"
@@ -48,17 +84,6 @@ def get_src_ips_that_communicated(dst_ip_address, filename='data_9.66.11.12_ipfl
             if item["destinationIPv4Address"] == dst_ip_address:
                 src_ips.add(item["sourceIPv4Address"])
     pprint(src_ips)
-
-
-def filter_ip_from_ip_flow(ip_address, input_file):
-    output_lines = []
-    with open(f'data_{ip_address}.json', 'w') as output_file:
-        with open(input_file, 'r') as jsonfile:
-            data = json.load(jsonfile)
-            for item in data:
-                if item["destinationIPv4Address"] == ip_address or item["sourceIPv4Address"] == ip_address:
-                    output_lines.append(data)
-        json.dump(output_lines, fp=output_file)
 
 
 def get_syslog_program_names(fromhost_ip, filename='data_10.7.101.12_syslog.json'):
@@ -190,3 +215,161 @@ syslog_item = \
 
 # 4.122.55.3 as dst IP communicated with
 # {'9.66.11.12'}
+
+# TODO
+# 1.) zobrat IP adresy pre jednu sestinu - pre jeden modry tim a vyfiltrovat IP toky
+# 2.) plus k tomu zobrat globalnu cast siete a MGMT
+# pozor, komunikacia medzi zariadeniami v ramci jedneho tymu - neunikatne adresy
+flow_ips = ['9.66.11.12', '9.66.11.13', '9.66.11.14', '10.1.2.22', '10.1.2.23', '10.1.2.24', '10.1.2.25', '10.1.2.26',
+            '10.1.2.28', '10.1.2.27', '10.1.2.29', '10.1.3.32', '10.1.3.33', '10.1.3.34', '10.1.4.46', '10.1.4.47',
+            '10.1.4.48', '10.1.4.49', '10.1.4.42', '10.1.4.43', '10.1.4.44', '10.1.4.45', '4.122.55.111',
+            '4.122.55.112', '4.122.55.113', '4.122.55.114', '4.122.55.115', '4.122.55.116', '4.122.55.117',
+            '4.122.55.2', '4.122.55.3', '4.122.55.4', '4.122.55.5', '4.122.55.6', '4.122.55.7', '4.122.55.21',
+            '4.122.55.22', '4.122.55.23', '4.122.55.24', '4.122.55.25', '4.122.55.26', '4.122.55.250']
+syslog_ips = ['10.7.101.12', '10.7.101.13', '10.7.101.25', '10.7.101.26', '10.7.101.28', '10.7.101.27', '10.7.101.29',
+              '10.7.101.34', '10.7.101.48', '10.7.101.49', '10.7.101.44', '10.7.101.45', '10.7.100.111', '10.7.100.112',
+              '10.7.100.113', '10.7.100.114', '10.7.100.115', '10.7.100.116', '10.7.100.2', '10.7.100.3', '10.7.100.4',
+              '10.7.100.5', '10.7.100.6', '10.7.100.7', '10.7.100.21', '10.7.100.22', '10.7.100.23', '10.7.100.24',
+              '10.7.100.25', '10.7.100.26']
+winlog_ips = ['10.7.101.14', '10.7.101.22', '10.7.101.23', '10.7.101.24', '10.7.101.32', '10.7.101.33', '10.7.101.46',
+              '10.7.101.47', '10.7.101.42', '10.7.101.43', '10.7.100.117', '10.7.100.250']
+
+# BLUE TEAM 1 ======================
+# Zoznam IP adries pre Blue Team 1
+# mail.firechmel.ex
+# 9.66.11.12
+# 10.7.101.12
+
+# dns.firechmel.ex
+# 9.66.11.13
+# 10.7.101.13
+
+# www.firechmel.ex
+# 9.66.11.14
+# 10.7.101.14
+
+# dc
+# 10.1.2.22
+# 10.7.101.22
+
+# files
+# 10.1.2.23
+# 10.7.101.23
+
+# backup
+# 10.1.2.24
+# 10.7.101.24
+
+# menu
+# 10.1.2.25
+# 10.7.101.25
+
+# db.chmel.ex
+# 10.1.2.26
+# 10.7.101.26
+
+# ups.chmel.ex
+# 10.1.2.28
+# 10.7.101.28
+
+# ocs.chmel.ex
+# 10.1.2.27
+# 10.7.101.27
+
+# monitoring
+# 10.1.2.29
+# 10.7.101.29
+
+# ops-desktop1
+# 10.1.3.32
+# 10.7.101.32
+
+# ops-desktop2
+# 10.1.3.33
+# 10.7.101.33
+
+# insider
+# 10.1.3.34
+# 10.7.101.34
+
+# admin1
+# 10.1.4.46
+# 10.7.101.46
+
+# admin2
+# 10.1.4.47
+# 10.7.101.47
+
+# admin3
+# 10.1.4.48
+# 10.7.101.48
+
+# admin4
+# 10.1.4.49
+# 10.7.101.49
+
+# desktop1
+# 10.1.4.42
+# 10.7.101.42
+
+# desktop2
+# 10.1.4.43
+# 10.7.101.43
+
+# desktop3
+# 10.1.4.44
+# 10.7.101.44
+
+# desktop4
+# 10.1.4.45
+# 10.7.101.45
+
+# CISCO ASA
+# 9.66.1.2
+
+# Global Gateway
+# 9.66.1.1
+
+# Flow Capture Interface
+# 4.122.55.254
+
+# GLOBAL ======================
+# red-team{1-6}
+# 4.122.55.111-116
+# 10.7.100.111-116
+
+# red-team7
+# 4.122.55.117
+# 10.7.100.117
+
+# global-web
+# 4.122.55.2
+# 10.7.100.2
+
+# global-dns
+# 4.122.55.3
+# 10.7.100.3
+
+# global-mail
+# 4.122.55.4
+# 10.7.100.4
+
+# desktop1
+# 4.122.55.5
+# 10.7.100.5
+
+# desktop2
+# 4.122.55.6
+# 10.7.100.6
+
+# global-app
+# 4.122.55.7
+# 10.7.100.7
+
+# test{1-6}
+# 4.122.55.21-26
+# 10.7.100.21-26
+
+# nagios-global
+# 4.122.55.250
+# 10.7.100.250
